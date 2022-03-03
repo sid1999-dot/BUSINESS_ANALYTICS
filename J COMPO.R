d8d7f1b304c6e5db32f2edf8aff40f3cb93a1efa
Siddharth Basu <sbasu868686@gmail.com>
2022-03-03 16:57:38 +0530

    K-means clustering
    This is the K means clustering type project we'll use for mall shopkeepers, persons, visitors in order to keep the maximum flow of account, price variabilities by data visualization techniques.

---
+++ J COMPO.R
@@ -0,0 +1,36 @@
+rm(list = ls())
+setwd("C:/Users/HP/Desktop/DATA ANALYTICS 3506/Mall-Customers-Segmentation-master")
+customer_data<-read.csv("customer_data.csv")
+
+str(customer_data)
+
+names(customer_data)
+head(customer_data)
+summary(customer_data$Age)
+sd(customer_data$Age)
+summary(customer_data$Annual.Income..k..)
+sd(customer_data$Annual.Income..k..)
+summary(customer_data$Age)
+
+summary(customer_data$Age)
+hist(customer_data$Age,
+     col="blue",
+     main="Histogram to Show Count of Age Class",
+     xlab="Age Class",
+     ylab="Frequency",
+     labels=TRUE)
+
+hist(customer_data$Spending.Score..1.100.,
+     main="HistoGram for Spending Score",
+     xlab="Spending Score Class",
+     ylab="Frequency",
+     col="#6600cc",
+     labels=TRUE)
+
+plot(density(customer_data$Annual.Income..k..),
+     col="yellow",
+     main="Density Plot for Annual Income",
+     xlab="Annual Income Class",
+     ylab="Density")
+polygon(density(customer_data$Annual.Income..k..),
+        col="#ccff66")
